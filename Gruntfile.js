2de464932cb9fc55e794d00fab1e6363fabecc24
paolonicodemo@gmail.com <bjornej>
2013-02-16 22:23:34 +0100

    added gruntfile and handling of sass compilation

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    sass: {                                 // Task
+        dist: {                             // Target
+            files: {                        // Dictionary of files
+                'metro.css': 'sass/MetroBootstrap.scss'   // 'destination': 'source'
+            }
+        }
+    },
+
+    watch: {
+      sass: {
+        files: ['sass/MetroBootstrap.scss','sass/navbar.scss','sass/reset.scss'],
+        tasks: ['sass']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task.
+  grunt.registerTask('default', ['sass']);
+
+};
